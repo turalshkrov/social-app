132a3ed9e2427479ffe0d16a6e04570702c09912
tural.shkrov <tural.shkrov@gmail.com>
2024-06-04 18:16:27 +0400

    setup: server config

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_SERVER_URL)
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+app.get('/', (req, res) => {
+  res.send('Welcome to API');
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => { console.log(`server running on port ${PORT}`) });
