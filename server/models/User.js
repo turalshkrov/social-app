b43aaf2cb4afb135a6fc1a839567cbd094da07ba
tural.shkrov <tural.shkrov@gmail.com>
2024-06-05 21:42:34 +0400

    feat: user model

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcyrpt = require('bcryptjs');
+
+const User = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  }
+}, { collection: 'User', versionKey: false });
+
+User.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcyrpt.hash(this.password, 8);
+  }
+  this.name = this.name.trim();
+  this.email = this.email.trim().toLowerCase();
+  this.username = this.username.trim().toLowerCase();
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('User', User);
