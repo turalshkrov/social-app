2795cf04e114f4bd2c29b312f517b211fc653a16
tural.shkrov <tural.shkrov@gmail.com>
2024-09-10 20:17:11 +0400

    server: fix: check username

--- server/middlewares/user/checkEmail.js
+++ server/middlewares/user/checkEmail.js
@@ -1,14 +1,17 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 const checkEmail = async (req, res, next) => {
-  try {
-    const { email } = req.body;
-    const user = await User.findOne({ email });
-    if (user) return res.json({ message: "Email is already registrated" });
-    return next();
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-}
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ email });
+        if (user)
+            return res
+                .status(400)
+                .json({ message: "Email is already registrated" });
+        return next();
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+};
 
 module.exports = checkEmail;
